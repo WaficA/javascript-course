e70a24793f04c0cc4b78a1a11d92df40a49be136
WaficA <wafic.abass@outlook.com>
2022-11-26 15:45:36 +0000

    .

--- 02-Fundamentals-Part-2/Explanation.js
+++ 02-Fundamentals-Part-2/Explanation.js
@@ -73,3 +73,71 @@ function NewFruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(NewFruitProcessor(2, 3));
+
+//arrays
+/*
+const newFriends = new Array("thomas", "victor", "tria"); // another way (just for info)
+console.log(newFriends);
+console.log(typeof newFriends); */
+
+const friends = ["micheal", "steven", "peter"]; // use this
+console.log(friends);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "jay";
+console.log(friends);
+
+const firstName = "jonas";
+const jonas = [firstName, "davis", 2037 - 1992, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//exercise
+const calcAge4 = function (birthYear4) {
+  return 2037 - birthYear4;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age4 = calcAge4(years[0]);
+const age5 = calcAge4(years[1]);
+const age6 = calcAge4(years[years.length - 1]);
+console.log(age4, age5, age6);
+
+const ages = [
+  calcAge4(years[0]),
+  calcAge4(years[1]),
+  calcAge4(years[years.length - 1]),
+];
+console.log(ages);
+
+const friends2 = ["Natasha", "thor", "harry"];
+
+//add element to begining of array
+friends2.unshift("john");
+console.log(friends2);
+
+//remove from begining of array
+friends2.shift();
+console.log(friends);
+
+//add element to the end of array
+const newLength = friends2.push("thomas");
+console.log(friends2);
+console.log(newLength);
+
+//remove element from end
+const popped = friends2.pop();
+console.log(popped);
+console.log(friends2);
+
+// if -1 thats is not found
+console.log(friends2.indexOf("Natasha"));
+// will return true or false if ound not found
+console.log(friends2.includes("bob"));
+console.log(friends2.includes("Natasha"));
+
+if (friends2.includes("Natasha")) {
+  console.log("you have a friend called Natasha");
+}
